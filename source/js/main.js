c4b3583b1e5039b82ca0a211ea96c80724ece691
DizBars <DizBars@yandex.ru>
2024-01-21 23:55:00 +0300

    Day 6(3 h.)

--- source/js/main.js
+++ source/js/main.js
@@ -1,13 +1,14 @@
 import './utils/scroll-lock';
 import './utils/focus-lock';
 import {initAccordions} from './modules/init-accordion';
-import {initSliderHero} from './modules/init-slider';
+import {initSliderHero, initSliderPrograms} from './modules/init-slider';
 import './modules/burger-menu';
 
 window.addEventListener('DOMContentLoaded', () => {
   initSliderHero();
   window.addEventListener('load', () => {
     initAccordions();
+    initSliderPrograms();
   });
 });
 
--- source/js/modules/init-slider.js
+++ source/js/modules/init-slider.js
@@ -1,6 +1,7 @@
 import Swiper from 'swiper/bundle';
 
 const heroSlider = document.querySelector('.hero__swiper');
+const programsSlider = document.querySelector('.programs__swiper');
 
 const setSliderHero = () => new Swiper(heroSlider, {
   pagination: {
@@ -8,6 +9,9 @@ const setSliderHero = () => new Swiper(heroSlider, {
     clickable: true,
   },
   loop: true,
+  autoplay: {
+    delay: 3000,
+  },
   breakpoints: {
     1440: {
       allowTouchMove: false,
@@ -21,4 +25,35 @@ const initSliderHero = () => {
   }
 };
 
-export {initSliderHero};
+const setSliderPrograms = () => new Swiper(programsSlider, {
+  pagination: {
+    direction: 'horizontal',
+    el: '.programs__swiper-pagination',
+    clickable: true,
+  },
+  loop: false,
+  slidesPerGroup: 1,
+  spaceBetween: 32,
+  breakpoints: {
+    1440: {
+      allowTouchMove: false,
+      slidesPerView: '3',
+    },
+  },
+  navigation: {
+    nextEl: '.programs__button--next',
+    prevEl: '.programs__button--prev',
+  },
+  scrollbar: {
+    el: '.programs__scrollbar',
+    draggable: true,
+  },
+});
+
+const initSliderPrograms = () => {
+  if (document.body.contains(programsSlider)) {
+    setSliderPrograms();
+  }
+};
+
+export {initSliderHero, initSliderPrograms};

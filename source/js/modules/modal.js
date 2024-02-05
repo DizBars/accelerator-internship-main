f6b1c65f265cf8581b83782b11cedc98226868d4
DizBars <DizBars@yandex.ru>
2024-02-05 13:28:58 +0300

    Всё готово, остелось только подправить размер модалки на мобилке

--- source/js/modules/modal.js
+++ source/js/modules/modal.js
@@ -6,7 +6,7 @@ const initModal = () => {
   targetButtons.forEach((button) => {
     button.addEventListener('click', () => {
       modal.showModal();
-      document.body.classList.add('scroll-lock');
+      window.scrollLock.disableScrolling();
     });
   });
 
@@ -16,13 +16,13 @@ const initModal = () => {
     const isClickedOnBackDrop = target === modal;
     if (isClickedOnBackDrop) {
       modal.close();
-      document.body.classList.remove('scroll-lock');
+      window.scrollLock.enableScrolling();
     }
   }
 
   closeModalButton.addEventListener('click', () => {
     modal.close();
-    document.body.classList.remove('scroll-lock');
+    window.scrollLock.enableScrolling();
   });
 
 };
